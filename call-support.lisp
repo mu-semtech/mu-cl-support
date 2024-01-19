7405e02c1f1c6803a8d3e98b7f5bde5ed63eaf1a
Aad Versteden <madnificent@gmail.com>
2024-01-19 12:25:49 +0100

    Fix typo

--- call-support.lisp
+++ call-support.lisp
@@ -83,7 +83,7 @@
   (let ((script-name (hunchentoot:script-name*))
         (symbols (remove-if-not #'symbolp components))
         (regex (components-to-regex* components))
-        (variables-hash (make-hash-table :synchcronized t)))
+        (variables-hash (make-hash-table :synchronized t)))
     (loop for symbol in symbols
        for value in (regex-match-list regex script-name)
        do
